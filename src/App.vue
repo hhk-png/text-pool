20d55f02d934d8f046775c0680082373b8807cf3
hhk-png <fangluoabc@163.com>
2024-03-13 16:47:42 +0800

    feat: finish basic function

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,59 @@
 <script setup lang="ts">
-import TextPool from './components/TextPool.vue';
+import { onMounted, onUnmounted, ref } from 'vue';
+import TextPool from './components/TextPool.vue'
+
+// add char randomly
+const chars = "abcdefghijklmnopqrstuvwxyz";
+const randomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const generateStr = () => {
+  const str = Array.from({ length: randomInt(0, 15) }, () => chars[randomInt(0, chars.length)]).join("")
+  return str
+}
+
+const textPool = ref<typeof TextPool>()
+
+const addChar = (str: string) => {
+  textPool.value!.addChar(str);
+}
+let addCharInterval: number
+onMounted(() => {
+  addCharInterval = setInterval(() => {
+    addChar(generateStr());
+  }, 500);
+})
+onUnmounted(() => {
+  clearInterval(addCharInterval)
+})
 
 </script>
 
 <template>
-    <TextPool></TextPool>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
+  <TextPool ref="textPool"></TextPool>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
 </template>
 
 <style scoped>
 .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
 
 .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
 
 .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/components/TextPool.vue
+++ src/components/TextPool.vue
@@ -1,60 +1,78 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, defineProps } from "vue";
 
-class TextPool {
-    readonly strs = reactive<string[]>([]);
-    constructor() {
-    }
-
-    addChar(char: string) {
-        const lastStr = this.strs[this.strs.length - 1] || "";
-        const newStr = lastStr + char;
-        if (newStr.length <= 20) {
-            if (this.strs.length === 0) {
-                this.strs.push(newStr);
-            } else {
-                this.strs[this.strs.length - 1] = newStr;
-            }
-        } else {
-            this.strs.push(char);
-            if (this.strs.length > 3) {
-                this.strs.shift();
-            }
-        }
-    }
+interface Props {
+  containerWidth?: number;
+  containerHeight?: number;
+  fontsize?: number;
 }
 
-const textPool = new TextPool();
+const props = withDefaults(defineProps<Props>(), {
+  containerWidth: 600,
+  containerHeight: 200,
+  fontsize: 20,
+})
 
-setInterval(() => {
-    textPool.addChar("a" + Math.random().toFixed(2));
-}, 500);
+const {
+  containerWidth: WIDTH,
+  containerHeight: HEIGHT,
+  fontsize: FONTSIZE
+} = props
+
+
+// inner
+const PADDING = 5
+const NUMOFHORIZONTAL = Math.floor((WIDTH - PADDING * 2) / (FONTSIZE * 0.6))
+const NUMOFVERTICAL = Math.floor((HEIGHT - PADDING * 2) / (FONTSIZE * 1.5)) - 1
+
+const strs = reactive<string[]>([])
+
+const addChar = (char: string) => {
+  const lastStr = strs[strs.length - 1] || "";
+  const newStr = lastStr + char;
+  if (newStr.length <= NUMOFHORIZONTAL) {
+    if (strs.length === 0) {
+      strs.push(newStr);
+    } else {
+      strs[strs.length - 1] = newStr;
+    }
+  } else {
+    if (strs.length > NUMOFVERTICAL) {
+      strs.shift()
+    }
+    strs.push(char)
+  }
+}
+
+const strStyle = {
+  width: WIDTH + "px",
+  height: HEIGHT + "px",
+  padding: PADDING + "px",
+  fontSize: FONTSIZE + "px",
+};
+
+defineExpose({ addChar })
 
 
 </script>
 
 <template>
-    <div class="str">
-        <span v-for="(text, index) in textPool.strs " :key="text + index" class="block bgc-yellow" >{{ text }}</span>
-    </div>
+  <div :style="strStyle" class="str">
+    <span v-for="(text, index) in strs" :key="text + index" class="bgc-yellow">{{ text }}</span>
+  </div>
 </template>
 
 <style scoped>
 .str {
-    width: 200px;
-    height: 200px;
-    transition: transform 0.5s;
+  background-color: antiquewhite;
+  box-sizing: border-box;
 }
 
 .str span {
-    display: inline-block;
-}
-
-.block {
-    display: block;
+  display: inline-block;
 }
 
 .bgc-yellow {
-    background-color: yellow;
+  background-color: yellow;
 }
 </style>
