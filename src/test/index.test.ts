a2e98caa5c6cda749ae0773121115895ecd7eb0c
hhk-png <fangluoabc@163.com>
2024-03-15 09:44:57 +0800

    test: config vitest

---
+++ src/test/index.test.ts
@@ -0,0 +1,15 @@
+import { expect, it } from "vitest"
+import { mount } from "@vue/test-utils"
+import TextPool from "../components/TextPool.vue"
+
+const wrapper = mount(TextPool)
+
+it("should pass", () => {
+
+  expect(1).toBe(1)
+});
+
+it("test mount", () => {
+  expect(wrapper.html()).toContain("div")
+})
+
